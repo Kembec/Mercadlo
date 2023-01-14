38ea07ba9e67dcb64bcb6ddc08776f4ac5437fb6
Kembec <dev@kembec.com>
2023-01-13 19:19:39 -0500

    Client INIT

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,6 +8,15 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+// Fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faKey, faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faUser, faKey)
+
+// Components
+app.component('font-awesome-icon', FontAwesomeIcon);
+
 app.use(createPinia())
 app.use(router)
 
